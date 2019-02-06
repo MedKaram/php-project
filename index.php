395798268c398a77ffcf96f5a09fca0ce7356cc3
MedKaram <medkaram20@gmail.com>
2019-02-06 19:35:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+		<link href="../public/css/style.css" rel="stylesheet" type="text/css">
+		<title>Puissance 4</title>
+    </head>
+    <body>
+		<a href="views/index.php">Jouer</a>
+    </body>
+</html>
